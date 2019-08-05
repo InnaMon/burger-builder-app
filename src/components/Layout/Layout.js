3fd7b42ba39b7d5808b75fe5626951003fc1284f
Inna Mon <inna.mon@gmail.com>
2019-08-05 12:37:58 -0500

    created BurgerBuilder container and added some styles to Layout in order to have some margin

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import Aux from '../../hoc/Auxillary';
+import styles from './Layout.module.css';
 
 const layout = (props) => (
     <Aux>
         <div>
           Toolbar, SideDrawer, Dackdrop
         </div>
-        <main>
+        <main className={styles.Content}>
           {props.children}
         </main>
     </Aux>
