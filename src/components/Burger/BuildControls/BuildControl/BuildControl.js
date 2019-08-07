4fe1f480d859149f6d81c5d585e49cbcda3761a2
Inna Mon <inna.mon@gmail.com>
2019-08-07 17:40:40 -0500

    added addIngredients method to take in an ingredient type and add it to the burgerbuilder, along with its price

--- src/components/Burger/BuildControls/BuildControl/BuildControl.js
+++ src/components/Burger/BuildControls/BuildControl/BuildControl.js
@@ -4,7 +4,7 @@ import styles from './BuildControl.module.css';
 const buildControl = (props) => (
     <div className={styles.BuildControl}>
       <div className={styles.Label}>{props.label}</div>
-      <button className={styles.More}>Add</button>
+      <button className={styles.More} onClick={props.added}>Add</button>
       <button className={styles.Less}>Remove</button>
     </div>
 );
