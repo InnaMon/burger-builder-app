a5cc12471a9c64bd7eb5677992f36f8b59610b47
Inna Mon <inna.mon@gmail.com>
2019-08-09 14:44:49 -0500

    changed height of BuildControl when in desktop view to show more of the ingredient add on buttons

--- src/components/Burger/BuildControls/BuildControl/BuildControl.js
+++ src/components/Burger/BuildControls/BuildControl/BuildControl.js
@@ -5,12 +5,12 @@ const buildControl = (props) => (
     <div className={styles.BuildControl}>
       <div className={styles.Label}>{props.label} ${props.itemPrice.toFixed(2)}</div>
       <button 
-        className={styles.More} 
-        onClick={props.added} >Add</button>
-      <button 
         className={styles.Less} 
         onClick={props.removed}
         disabled={props.disabled}>Remove</button>
+        <button 
+        className={styles.More} 
+        onClick={props.added} >Add</button>
     </div>
 );
 
