2670ad8311f7e620ec6d8eb3d646eeda85511acb
Inna Mon <inna.mon@gmail.com>
2019-08-10 14:08:33 -0500

    added inline styles to OrderSummary to center content and added margin to the li elements so that the dots are sitting closer to the text. Added Backdrop componenet inside of Modal and passed the show props

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -4,20 +4,22 @@ import Aux from '../../../hoc/Auxillary';
 const orderSummary = (props) => {
     const ingredientSummary = Object.keys(props.ingredients)
         .map(igKey => {
-            return (<li key={igKey}>
+            return (<li style={{margin: '0 150px 0 150px',}} key={igKey}>
                 <span style={{textTransform: 'capitalize'}}>{igKey}</span>
                 : {props.ingredients[igKey]}
             </li>);
         });
 
     return (
-        <Aux style={{textAlign: 'center'}}>
-            <h3>Your Order</h3>
-            <p>A delicious burger with the following ingredients:</p>
-            <ul>
-              {ingredientSummary}
-            </ul>
-            <p>Continue to Checkout?</p>
+        <Aux>
+            <div style={{textAlign: 'center'}}>
+                <h3>Your Order</h3>
+                <p>A delicious burger with the following ingredients:</p>
+                <ul>
+                {ingredientSummary}
+                </ul>
+                <p>Continue to Checkout?</p>
+            </div>
         </Aux>
     )
 }
