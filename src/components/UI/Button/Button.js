2bcf4ea3f77b62aeaf323bc1b33261a3e43a4a28
Inna Mon <inna.mon@gmail.com>
2019-08-10 16:52:13 -0500

    created a button component to use throughout the project

---
+++ src/components/UI/Button/Button.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import styles from './Button.module.css';
+import { arrayExpression } from '@babel/types';
+
+const button = (props) => (
+    <button
+    className={[styles.Button, styles[props.btnType]].join(' ')}
+      onClick={props.clicked}>
+        {props.children}
+    </button>
+);
+
+export default button;
+
+// className has to take in a string
+// in order to pass multiple values into className here and make it dynamic, are passing multiple values in an array. 
+// styles.Button and style[props.btnType] are both using property accessors (dot or bracket notation)
+// btnType will be an outside value we can set and pass into this component
+// we are then using the join() method to make the className value a proper string in order to be utilized 
