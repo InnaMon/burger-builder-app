341a02f89057c302b4ca30d05afdea7a3122acd2
Inna Mon <inna.mon@gmail.com>
2019-08-17 17:30:57 -0500

    added nav items to toolbar that wrap an individual nav item with its own styles. Will add routing to nav items later

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import styles from './Toolbar.module.css';
 import Logo from '../../Logo/Logo';
+import NavItems from '../NavItems/NavItems';
 
 const toolbar = (props) => (
     <header className={styles.Toolbar}>
         <div>MENU</div>
         <Logo />
         <nav>
-            <ul>...</ul>
+            <NavItems />
         </nav>
     </header>
 );
