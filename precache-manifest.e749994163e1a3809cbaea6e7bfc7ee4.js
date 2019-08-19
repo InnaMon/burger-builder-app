c47fc955cc5a57c989177cc7170ad2f64136f9f9
Inna Mon <inna.mon@gmail.com>
2019-08-19 17:07:10 -0500

    Updates

---
+++ precache-manifest.e749994163e1a3809cbaea6e7bfc7ee4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ebad0d8a6e3d6c74eacc28b8e6f2a79b",
+    "url": "/burger-builder-app/index.html"
+  },
+  {
+    "revision": "1e542c94d9a66512ea74",
+    "url": "/burger-builder-app/static/css/main.46d02999.chunk.css"
+  },
+  {
+    "revision": "984b1e07d70e8c40c81d",
+    "url": "/burger-builder-app/static/js/2.882d9d05.chunk.js"
+  },
+  {
+    "revision": "1e542c94d9a66512ea74",
+    "url": "/burger-builder-app/static/js/main.e503fcfb.chunk.js"
+  },
+  {
+    "revision": "6070e0d12c31f3855792",
+    "url": "/burger-builder-app/static/js/runtime~main.e4fa35f2.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/burger-builder-app/static/media/burger-logo.b8503d26.png"
+  }
+]);
