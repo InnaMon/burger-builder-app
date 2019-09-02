d2c86131a603a83e212307e704b688c3beb108ec
Inna Mon <inna.mon@gmail.com>
2019-09-02 17:21:02 -0500

    added NavLinks to the NavItem and adjusted styles to ensure the active class is working

--- src/components/Navigation/NavItems/NavItem/NavItem.js
+++ src/components/Navigation/NavItems/NavItem/NavItem.js
@@ -1,12 +1,10 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 import styles from './NavItem.module.css';
 
 const navItem = (props) => (
     <li className={styles.NavItem}>
-        <a 
-        href={props.link}
-        className={props.active ? styles.active : null}>
-        {props.children}</a>
+        <NavLink to={props.link} exact activeClassName={styles.active}>{props.children}</NavLink>
     </li>
 );
 
